5a2b1eec4bf352d4796202d45923efd412c9156e
asus-nb\admin <inessa134@gmail.com>
2022-05-04 16:00:54 +0300

    task1

---
+++ lesson11Task1.js
@@ -0,0 +1,25 @@
+/*   task 1 hw 11
+let firstNumber = +prompt('first number')
+let secondNumber =+prompt('second number')
+if( secondNumber - firstNumber >= 5){
+for (p = firstNumber; p <= secondNumber; p++){
+    console.log(p)
+} 
+
+
+
+} else if(firstNumber > secondNumber){
+   for( p = firstNumber; p <= secondNumber; p++){
+        console.log(p)
+}
+}
+
+
+else{
+do{ firstNumber = +prompt('first number')
+    secondNumber =+prompt('second number')
+}while(
+    secondNumber - firstNumber <= 5
+)
+}
+*/
