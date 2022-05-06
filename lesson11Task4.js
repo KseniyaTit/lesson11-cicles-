2a7f21e29e5376cb15b41f75e254d84cfeb9db45
asus-nb\admin <inessa134@gmail.com>
2022-05-06 14:22:25 +0300

    task 4

---
+++ lesson11Task4.js
@@ -0,0 +1,10 @@
+let result = 0
+function br (num1,num2){
+    for(let i = num1; i<=num2; i++ ){
+        result = result + 1;
+        if (result>7) break;
+        console.log(result)
+       
+
+    }
+}
